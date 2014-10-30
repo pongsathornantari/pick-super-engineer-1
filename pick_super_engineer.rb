f70d083509cc097b1fcbdee58aae97ddded844a1
VemZa <aaa@vemza.com>
2014-10-30 06:06:10 -0300

    featureC

--- pick_super_engineer.rb
+++ pick_super_engineer.rb
@@ -8,14 +8,27 @@ def pick_engineer()
 end
 
 def featureA(pick_engineer)
-    puts pick_engineer
-    puts pick_engineer.to_s.gsub('','.')
-end
-featureA(pick_engineer)
-def featureB()
 
 end
 
-def featureC()
+def featureB(pick_engineer)
 
 end
+
+def featureC(search_string)
+	if (search_string.length >= 3)
+		engineers = Array.new()
+		engineers = File.readlines("Engineer.txt")
+		engineers.each do |engineer|
+			if (engineer.include? search_string)
+				#puts engineer
+				featureA(engineer)
+				featureB(engineer)
+			end
+		end
+	else
+		puts "Please use 3 or more character."
+	end
+
+end
+featureC("ana")
